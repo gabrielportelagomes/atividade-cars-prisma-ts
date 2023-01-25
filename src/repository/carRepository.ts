c877849e832e3d41824f0980f6f6ec5adfae9438
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-01-25 20:13:35 -0300

    refactor: get car by id

--- src/repository/carRepository.ts
+++ src/repository/carRepository.ts
@@ -1,4 +1,4 @@
-import db from "../config/database.js";
+import prisma from "../config/database";
 
 async function getCars() {
   const data = await db.query(`SELECT * FROM cars`);
@@ -6,16 +6,24 @@ async function getCars() {
 }
 
 async function getCar(id: number) {
-  const data = await db.query(`SELECT * FROM cars WHERE id = $1`, [id]);
-  return data.rows[0];
+  const data = await prisma.cars.findUnique({
+    where: { id },
+  });
 }
 
 async function getCarWithLicensePlate(licensePlate: string) {
-  const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
+  const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [
+    licensePlate,
+  ]);
   return data.rows[0];
 }
 
-async function createCar(model: string, licensePlate: string, year: number, color: string) {
+async function createCar(
+  model: string,
+  licensePlate: string,
+  year: number,
+  color: string
+) {
   await db.query(
     `INSERT INTO cars (model, "licensePlate", year, color)
      VALUES ($1, $2, $3, $4)`,
@@ -32,7 +40,7 @@ const carRepository = {
   getCarWithLicensePlate,
   getCars,
   createCar,
-  deleteCar
-}
+  deleteCar,
+};
 
 export default carRepository;
